caf1c03b11d7d12475948888ba4c3d6a401ae072
Togosh <Togosh>
2025-11-02 07:56:52 -0600

    - reverse hunk

--- src/const/swap.ts
+++ src/const/swap.ts
@@ -1,10 +1,10 @@
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 
-export const AffiliateRouterAddress = "0x3c400d6966F8539798f88C4dDe50cF082aBA3037";
+export const AffiliateRouterAddress = "0x7872B42710294ce16fEA60575da45Fde51db78e8";
 export const BridgeManagerAddress =
   "0x1715a3E4A142d8b698131108995174F37aEBA10D";
 export const BridgeManagerAddressForNative =
   "0x8AC4ae65b3656e26dC4e0e69108B392283350f55";
 
-//export const BackendURL = "https://pulsechainramp.com/";
-export const BackendURL = "http://localhost:3000/";
+export const BackendURL = "https://pulsechainramp.com/";
+//export const BackendURL = "http://localhost:3000/";
