054b638ac4ab2508cee9b1d635e95ed219cb2885
Togosh <Togosh>
2025-11-07 01:12:51 -0600

    - add noopener/noreferrer when opening explorer links in activity cards

--- src/pages/Activity/components/TransactionCard.tsx
+++ src/pages/Activity/components/TransactionCard.tsx
@@ -286,7 +286,7 @@ const TransactionCard: React.FC<TransactionCardProps> = ({
     }
 
     if (explorerUrl) {
-      window.open(explorerUrl, "_blank");
+      window.open(explorerUrl, "_blank", "noopener,noreferrer");
     }
   };
 
