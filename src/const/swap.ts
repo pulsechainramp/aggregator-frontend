3e6201c4c1c0f053982bdef5b9912e525c6b94f5
Togosh <Togosh>
2025-11-09 20:02:38 -0600

    - set to new contract

--- src/const/swap.ts
+++ src/const/swap.ts
@@ -1,6 +1,6 @@
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 
-export const AffiliateRouterAddress = "0x7872B42710294ce16fEA60575da45Fde51db78e8";
+export const AffiliateRouterAddress = "0xCf2B8a55c86790Ab8F04873033a6e46be99658a7";
 export const BridgeManagerAddress =
   "0x1715a3E4A142d8b698131108995174F37aEBA10D";
 export const BridgeManagerAddressForNative =
